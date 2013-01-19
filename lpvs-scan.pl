6812ade7695a40a1fe3ac56ef23f951700538c68
Lars Windolf <lars.lindner@gmail.com>
2013-01-19 17:22:17 +0100

    Add GPLv2 license header.

--- lpvs-scan.pl
+++ lpvs-scan.pl
@@ -1,12 +1,25 @@
 #!/usr/bin/perl -w
 
-# Version 0.2, 08.11.2012
+# Version 0.2.1, 19.01.2013
 #
-# Copyright (c) 2012  Lars Windolf <lars.lindner@gmail.com>
+# Copyright (c) 2012-2013  Lars Windolf <lars.lindner@gmail.com>
 # Copyright (C) 2004-2010 John Peacock <jpeacock@cpan.org>
 #
-# Licensed under the same terms as Perl. The versioncmp()
-# method is from the CPAN Version.pm from John Peacock.
+# The versioncmp() method is from the CPAN Version.pm from John Peacock.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 use strict;
 use Term::ANSIColor;
